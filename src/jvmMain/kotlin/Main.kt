7238514051f8bd8d452a639a765c221020d1d636
Сергей Булавкин <bulavkinsm@vtblife.ru>
2023-03-09 02:38:12 +0300

    Simple chooser

--- src/jvmMain/kotlin/Main.kt
+++ src/jvmMain/kotlin/Main.kt
@@ -1,31 +1,57 @@
-import androidx.compose.material.MaterialTheme
 import androidx.compose.desktop.ui.tooling.preview.Preview
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Button
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
+import javax.swing.JFileChooser
 
 @Composable
 @Preview
 fun App() {
-    var text by remember { mutableStateOf("Hello, World!") }
+    val text by remember { mutableStateOf("Select source folder") }
+    var sourceDir by remember { mutableStateOf("") }
 
     MaterialTheme {
-        Button(onClick = {
-            text = "Hello, Desktop!"
-        }) {
-            Text(text)
+        Column {
+            Button(
+                onClick = {
+                    val f = JFileChooser("/")
+                        .apply {
+                            fileSelectionMode = JFileChooser.DIRECTORIES_ONLY
+                            dialogTitle = "Selects source folder"
+                            approveButtonText = "Select"
+                            approveButtonToolTipText = "Selects source folder"
+                        }
+                    f.showOpenDialog(null)
+                    sourceDir = f.selectedFile.absolutePath
+                },
+                modifier = Modifier.padding(20.dp)
+            ) {
+                Text(text)
+            }
         }
+        Column {
+            Text(sourceDir)
+        }
+
     }
 }
 
 fun main() = application {
-    Window(onCloseRequest = ::exitApplication) {
+    Window(
+        title = "Memonizer",
+        onCloseRequest = ::exitApplication
+    ) {
         App()
     }
 }
