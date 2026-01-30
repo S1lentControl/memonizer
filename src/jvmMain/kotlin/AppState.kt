193dcfcb4305f10b1ecdbda7db90adbddccc7166
Sergey <sbylavkin@gmail.com>
2026-01-30 16:27:49 +0300

    Add AppState improvements and validation

--- src/jvmMain/kotlin/AppState.kt
+++ src/jvmMain/kotlin/AppState.kt
@@ -1,6 +1,7 @@
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
+import java.io.File
 import java.util.concurrent.atomic.AtomicLong
 
 class AppState {
@@ -10,4 +11,25 @@ class AppState {
     var totalVideos: Long? by mutableStateOf(null)
     var isPrepared: Boolean by mutableStateOf(false)
     val processedFiles: AtomicLong by mutableStateOf(AtomicLong(0))
+    var errorMessage: String? by mutableStateOf(null)
+    
+    fun isSourceDirValid(): Boolean {
+        val dir = sourceDir ?: return false
+        val file = File(dir)
+        return file.exists() && file.isDirectory && file.canRead()
+    }
+    
+    fun isDestDirValid(): Boolean {
+        val dir = destDir ?: return false
+        val file = File(dir)
+        return file.exists() && file.isDirectory && file.canWrite()
+    }
+    
+    fun reset() {
+        totalImages = null
+        totalVideos = null
+        isPrepared = false
+        processedFiles.set(0)
+        errorMessage = null
+    }
 }
